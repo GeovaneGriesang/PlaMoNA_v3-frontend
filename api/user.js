88080530d4979db8b0c50e1de39c00946c5ad73f
cocacolacomvodka <joaogabrielde.almeida@hotmail.com>
2023-09-27 22:28:22 -0300

    conectando com a api em deploy

--- api/user.js
+++ api/user.js
@@ -11,7 +11,7 @@ class User{
 // Função para enviar os dados de registro para o servidor
   async function registerUser ( nome, cpf, endereco, email, telefone, alerta_sms, alerta_email, senha ) {
     try {
-      const response = await axios.post('http://172.22.192.1:4000/api/register', { nome, cpf, endereco, email, telefone, alerta_sms, alerta_email, senha });
+      const response = await axios.post('https://plamona.onrender.com/api/register', { nome, cpf, endereco, email, telefone, alerta_sms, alerta_email, senha });
       console.log(response.data.message); 
     } catch (error) {
       console.error('Erro durante o registro:', error);
@@ -20,7 +20,7 @@ class User{
 
   async function loginUser ( email, senha ) {
     try {
-      const response = await axios.post('http://172.22.192.1:4000/api/login', { email, senha });
+      const response = await axios.post('https://plamona.onrender.com/api/login', { email, senha });
       alert(response.data.message); 
       console.log(response.data.message);
       return(response.data.loginState)
